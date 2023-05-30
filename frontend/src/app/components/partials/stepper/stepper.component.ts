43994e85b6248e325e91bda5507449f940b6d032
mmich-pl <m.michal.parzych@gmail.com>
2023-05-30 16:17:50 +0200

    move stepper

--- frontend/src/app/components/partials/stepper/stepper.component.ts
+++ frontend/src/app/components/partials/stepper/stepper.component.ts
@@ -8,7 +8,7 @@ import {Component, OnInit, Input} from '@angular/core';
 export class StepperComponent  implements OnInit {
   @Input() currentStep!: number;
 
-  steps :  Map<number, string> = new Map([[1, "Client Data"], [2, "Address"], [3, "Load"]]);
+  steps :  Map<number, string> = new Map([[1, "Client Data"], [2, "Address"], [3, "Load"], [4, "Set"], [5,"Result"]]);
 
   ngOnInit(): void {}
 }
