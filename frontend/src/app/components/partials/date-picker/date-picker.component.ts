527191772f939f5cd65fc8224f3b54d16cfdef84
mmich-pl <m.michal.parzych@gmail.com>
2023-05-31 10:25:13 +0200

    create datepicker component

---
+++ frontend/src/app/components/partials/date-picker/date-picker.component.ts
@@ -0,0 +1,87 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css']
+})
+export class DatePickerComponent {
+  MONTH_NAMES = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  showDatepicker = false;
+  datepickerValue!: string;
+  month!: number;
+  current_month!: number;
+  year!: number;
+  no_of_days = [] as number[];
+  blankDays = [] as number[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.initDate();
+    this.getNoOfDays();
+  }
+
+  initDate() {
+    let today = new Date();
+    this.month = this.current_month = today.getMonth();
+    this.year = today.getFullYear();
+    this.datepickerValue = new Date(this.year, this.month, today.getDate()).toDateString();
+  }
+
+  isToday(date: any) {
+    const today = new Date();
+    const d = new Date(this.year, this.month, date);
+    return today.toDateString() === d.toDateString();
+  }
+
+  alreadyPassed(date: any) {
+    const today = new Date();
+    const d = new Date(this.year, this.month, date);
+    return  d < today;
+  }
+
+  getDateValue(date: any) {
+    let selectedDate = new Date(this.year, this.month, date);
+    this.datepickerValue = selectedDate.toDateString();
+    this.showDatepicker = false;
+  }
+
+  getNoOfDays() {
+    let i;
+    const daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
+
+    // find where to start calendar day of week
+    let dayOfWeek = new Date(this.year, this.month).getDay();
+    let blankdaysArray = [];
+    for (i = 1; i <= dayOfWeek; i++) {
+      blankdaysArray.push(i);
+    }
+
+    let daysArray = [];
+    for (i = 1; i <= daysInMonth; i++) {
+      daysArray.push(i);
+    }
+
+    this.blankDays = blankdaysArray;
+    this.no_of_days = daysArray;
+  }
+
+  trackByIdentity = (index: number, item: any) => item;
+
+}
