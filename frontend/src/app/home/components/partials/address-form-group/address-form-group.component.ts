fc5449c9388307346fd37706f74d74c9d892c24d
mmich-pl <m.michal.parzych@gmail.com>
2023-06-03 16:08:16 +0200

    separate address part of form to new component

---
+++ frontend/src/app/home/components/partials/address-form-group/address-form-group.component.ts
@@ -0,0 +1,15 @@
+import { Component ,OnInit, Input} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-address-form-group',
+  templateUrl: './address-form-group.component.html',
+  styleUrls: ['./address-form-group.component.css']
+})
+export class AddressFormGroupComponent implements OnInit{
+@Input() form!:FormGroup;
+@Input() countries!:string[];
+
+  ngOnInit(): void {
+  }
+}
