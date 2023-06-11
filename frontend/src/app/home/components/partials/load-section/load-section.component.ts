f3df308a741995a9d46b644a0a6e2733dd676fef
mmich-pl <m.michal.parzych@gmail.com>
2023-06-11 15:21:58 +0200

    change load logic - now single carriage can move only cargo of same category

--- frontend/src/app/home/components/partials/load-section/load-section.component.ts
+++ frontend/src/app/home/components/partials/load-section/load-section.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {Cargo} from "../../../../core/models/cargo";
+import {Cargo, CargoType, CargoTypeKey} from "../../../../core/models/cargo";
 import {Observable} from "rxjs";
 import {CargoService} from "../../../../core/services/cargo.service";
 import {SelectedCargoService} from "../../../../core/services/selected-cargo.service";
@@ -14,21 +14,31 @@ export class LoadSectionComponent implements OnInit {
 
   @Input() form!: FormGroup;
   @Input() formArray!: FormArray;
-  cargo= new Array<Cargo>();
+  selectedType = new FormControl();
+  types = new Array<CargoTypeKey>()
+  allCargo = new Array<Cargo>();
+  filteredCargo = new Array<Cargo>();
   selectedCargo = new Map<Cargo, number>();
 
   constructor(private cargoService: CargoService, private selectedCargoService: SelectedCargoService) {
-
   }
 
   ngOnInit(): void {
     this.cargoService.get().subscribe((t) => {
-      console.log(t);
-      this.cargo = t;
-      console.log(this.cargo);
+      this.allCargo = t;
+      this.allCargo.forEach(c => {
+        if (!this.types.includes(c.type_name)) {
+          this.types.push(c.type_name)
+        }
+      })
     });
+
+    this.selectedType.valueChanges.subscribe(v => {
+      this.filteredCargo = this.allCargo.filter(c => c.type_name == this.selectedType.value)
+      this.selectedCargo.clear();
+    });
+
     this.formArray?.valueChanges.subscribe(values => {
-      console.log(values);
       this.selectedCargo.clear();
       const ctrl = <FormArray>this.formArray;
       ctrl.controls.forEach(x => {
@@ -43,8 +53,6 @@ export class LoadSectionComponent implements OnInit {
           }
         }
       })
-      console.log(`child: ${this.selectedCargo.size}`);
-      this.selectedCargo.forEach((v,k)=>{console.log(k.name)});
       this.selectedCargoService.passMap(this.selectedCargo);
     });
   }
