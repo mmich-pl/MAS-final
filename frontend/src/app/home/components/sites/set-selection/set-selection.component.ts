bc214b0014dbfdbdf428bbddca127ecd6d84bd23
mmich-pl <m.michal.parzych@gmail.com>
2023-06-11 15:33:48 +0200

    rework set-selection part of form

--- frontend/src/app/home/components/sites/set-selection/set-selection.component.ts
+++ frontend/src/app/home/components/sites/set-selection/set-selection.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit,} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output,} from '@angular/core';
 import {Cargo} from "../../../../core/models/cargo";
 import {Route} from "../../../../core/models/route";
 import {Trailer, Truck} from "../../../../core/models/truck";
@@ -11,8 +11,15 @@ import {LoaderService} from "../../../../core/services/loader.service";
 import {ModalService} from "../../../../core/services/modal.service";
 import {Address} from "../../../../core/models/address";
 
-
-type set = { cargo: Cargo, amount: number, trailer: Trailer, truck: Truck, driver: Driver };
+type set = {
+  cargo: Cargo,
+  amount: number,
+  trailer?: Trailer,
+  truck?: Truck,
+  driver?: Driver,
+  pickup_address?: Address,
+  drop_address?: Address,
+};
 
 @Component({
   selector: 'app-set-selection',
@@ -21,10 +28,12 @@ type set = { cargo: Cargo, amount: number, trailer: Trailer, truck: Truck, drive
 })
 export class SetSelectionComponent implements OnInit {
   @Input() cargo!: Map<Cargo, number>;
-  @Input() route!: Route;
   @Input() startDate!: string;
+  @Input() duration!: number;
   @Input() changePage!: (isNextPage: boolean) => number | undefined;
   @Input() addresses!: Array<Address>;
+  @Output() setEmitter: EventEmitter<any> = new EventEmitter<any>();
+
   endDate = "";
   trucks = new Array<Truck>();
   trailers: Array<Trailer> | undefined;
@@ -36,59 +45,64 @@ export class SetSelectionComponent implements OnInit {
   }
 
   constructSets() {
+    const driverObservables = new Array<Observable<Driver[]>>();
 
-    let i = 0;
     for (let [cargo, amount] of this.cargo) {
-      let trailers = this.trailers?.filter((t) => cargo.type?.getTrailers().includes(t))
-      let drivers = this.drivers.get(cargo.required_licence);
-      if (trailers != undefined && drivers != null) {
+      let trailers = this.trailers?.filter((t) => cargo.type?.getTrailers().includes(t)).sort();
+      if (trailers != undefined) {
         let min = trailers.reduce((prev, current) => {
           return (prev > current.carrying_capacity) ? prev : current.carrying_capacity;
         }, 0);
-
         for (let j = 0; j < trailers.length; j++) {
           this.sets.push(
             {
               cargo: cargo,
-              amount: (amount > min) ? trailers[j].carrying_capacity : amount,
-              trailer: trailers[j],
-              truck: this.trucks[i],
-              driver: drivers[j]
+              amount: (amount >= min) ? trailers[j].carrying_capacity : amount,
             }
           )
           amount -= trailers[j].carrying_capacity;
-          i += (j + 1);
+          if (amount <= 0) break;
         }
       }
     }
+
+    const cargoCountMap: Map<Cargo, number> = this.sets.reduce((cargoMap, set) => {
+      const count = cargoMap.get(set.cargo) || 0;
+      cargoMap.set(set.cargo, count + 1);
+      return cargoMap;
+    }, new Map<Cargo, number>());
+
+    for (let [cargo, amount] of cargoCountMap) {
+      const driverObservable = this.driverService.getMatchingDrivers(this.startDate, this.endDate,
+        (cargo.required_licence != Licences.NotRequired) ? cargo.required_licence : "", amount);
+      driverObservables.push(driverObservable);
+    }
+
+    forkJoin([...driverObservables]).subscribe((v) => {
+      let i = 0;
+      this.cargo.forEach((a, index) => {
+        this.drivers.set(index.required_licence, v[i++]!);
+      });
+    })
   }
 
-
   ngOnInit(): void {
-    this.endDate = new Date(new Date(this.startDate).getTime() + this.route!.duration * 1000).toISOString();
+    let end = new Date(new Date(this.startDate).getTime() + this.duration * 1000);
+    this.endDate = end.toISOString();
 
     const truckObservable = this.truckService.getWithinDates(this.startDate, this.endDate);
     const trailerObservable = this.trailerService.getMatchingTrailers(this.startDate, this.endDate, this.cargo);
-    const driverObservables = new Array<Observable<Driver[]>>();
 
-    this.cargo.forEach((value, key) => {
-      console.log(value, key, key.required_licence);
-      const driverObservable = this.driverService.getMatchingDrivers(this.startDate, this.endDate,
-        (key.required_licence === Licences.NotRequired) ? "" : key.required_licence);
-      driverObservables.push(driverObservable);
-    });
-
-    forkJoin([truckObservable, trailerObservable, ...driverObservables,])
-      .subscribe(([trucks, trailers, ...drivers]) => {
+    forkJoin([truckObservable, trailerObservable])
+      .subscribe(([trucks, trailers]) => {
         this.trucks = trucks;
         this.trailers = trailers;
-        let i = 0;
-        this.cargo.forEach((a, index) => {
-          this.drivers.set(index.required_licence, drivers[i]!);
-        });
         this.constructSets();
         this.loaderService.setLoading(false);
       });
+  }
 
+  sendDataToParent() {
+    this.setEmitter.emit(this.sets);
   }
 }
